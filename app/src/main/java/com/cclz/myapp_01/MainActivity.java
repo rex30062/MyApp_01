c5f1797ea28b98ebe0fd66c044660c8c84cfe2b5
Cclz <rex6206200>
2017-02-23 17:25:31 +0800

    test1

--- app/src/main/java/com/cclz/myapp_01/MainActivity.java
+++ app/src/main/java/com/cclz/myapp_01/MainActivity.java
@@ -7,7 +7,7 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-
+//test1
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
